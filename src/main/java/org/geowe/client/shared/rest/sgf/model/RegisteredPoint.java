294f296f1befd2d487a19678caa49fde9cc5dfe3
Rafael López Torres <rltsistemas@gmail.com>
2017-09-01 14:22:09 +0200

    added model class

---
+++ src/main/java/org/geowe/client/shared/rest/sgf/model/RegisteredPoint.java
@@ -0,0 +1,129 @@
+package org.geowe.client.shared.rest.sgf.model;
+
+import java.util.Arrays;
+
+import org.jboss.errai.common.client.api.annotations.Portable;
+
+/**
+ * Representa a un punto registrado por el servicio de captura de datos
+ * 
+ * Ejemplo de json devuelto (puede qeu cambie): { "_embedded": {
+ * "registeredPointResourceList": [ { "imei": "test imei", "date": [ 2017, 9, 1,
+ * 0, 0 ], "speed": "0", "datos": "AAAAAQ==", "position": "POINT (0 0)",
+ * "_links": { "self": { "href": "http://127.0.0.1:8081/registeredpoints/1" } }
+ * } ] }, "_links": { "self": { "href":
+ * "http://127.0.0.1:8081/registeredpoints?page=0&size=20" } }, "page": {
+ * "size": 20, "totalElements": 1, "totalPages": 1, "number": 0 } }
+ * 
+ * @author rltorres
+ *
+ */
+@Portable
+public class RegisteredPoint {
+
+
+	private String imei;
+
+	/*
+	 * TODO: java 8 lo envía como array de enteros ver como mapearlo [year,
+	 * month, day, hour, min, sec]
+	 */
+	private Integer[] date;
+
+	/*
+	 * Velocidad en km/h
+	 */
+	private String speed;
+
+	/*
+	 * TODO: almacenado como jsonb por mapear
+	 */
+	private String datos;
+	/*
+	 * Punto en WKT
+	 */
+	private String positionWkt;
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public Integer[] getDate() {
+		return date;
+	}
+
+	public void setDate(Integer[] date) {
+		this.date = date;
+	}
+
+	public String getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+
+	public String getDatos() {
+		return datos;
+	}
+
+	public void setDatos(String datos) {
+		this.datos = datos;
+	}
+
+	public String getPositionWkt() {
+		return positionWkt;
+	}
+
+	public void setPositionWkt(String positionWkt) {
+		this.positionWkt = positionWkt;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(date);
+		result = prime * result + ((imei == null) ? 0 : imei.hashCode());
+		result = prime * result
+				+ ((positionWkt == null) ? 0 : positionWkt.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RegisteredPoint other = (RegisteredPoint) obj;
+		if (!Arrays.equals(date, other.date))
+			return false;
+		if (imei == null) {
+			if (other.imei != null)
+				return false;
+		} else if (!imei.equals(other.imei))
+			return false;
+		if (positionWkt == null) {
+			if (other.positionWkt != null)
+				return false;
+		} else if (!positionWkt.equals(other.positionWkt))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "RegisteredPoint [imei=" + imei + ", date="
+				+ Arrays.toString(date) + ", speed=" + speed + ", datos="
+				+ datos + ", positionWkt=" + positionWkt + "]";
+	}
+
+}
--- src/main/java/org/geowe/client/shared/rest/sgf/model/SgfCompany.java
+++ src/main/java/org/geowe/client/shared/rest/sgf/model/SgfCompany.java
@@ -3,66 +3,124 @@ package org.geowe.client.shared.rest.sgf.model;
 import org.jboss.errai.common.client.api.annotations.Portable;
 
 /**
- * Representa una compañía  del Sistema de Seguimiento de Flotas
+ * Representa una Empresa del Sistema de Seguimiento de Flotas
+ * 
  * @author lotor
  *
  */
 @Portable
 public class SgfCompany {
 	
+	private int id;
+
 	private String cif;
+
 	private String name;
+
+	private String postalCode;
+
+	private String email;
+
+	private String web;
+
+	private String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getCif() {
 		return cif;
 	}
+
 	public void setCif(String cif) {
 		this.cif = cif;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getWeb() {
+		return web;
+	}
+
+	public void setWeb(String web) {
+		this.web = web;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((cif == null) ? 0 : cif.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (getClass() != obj.getClass()) {
+		if (getClass() != obj.getClass())
 			return false;
-		}
 		SgfCompany other = (SgfCompany) obj;
 		if (cif == null) {
-			if (other.cif != null) {
+			if (other.cif != null)
 				return false;
-			}
-		} else if (!cif.equals(other.cif)) {
+		} else if (!cif.equals(other.cif))
 			return false;
-		}
-		if (name == null) {
-			if (other.name != null) {
+		if (email == null) {
+			if (other.email != null)
 				return false;
-			}
-		} else if (!name.equals(other.name)) {
+		} else if (!email.equals(other.email))
 			return false;
-		}
+		if (id != other.id)
+			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "SgfCompany [cif=" + cif + ", name=" + name + "]";
+		return "SgfCompany [id=" + id + ", cif=" + cif + ", name=" + name
+				+ ", postalCode=" + postalCode + ", email=" + email + ", web="
+				+ web + ", status=" + status + "]";
 	}
+
 	
 }
