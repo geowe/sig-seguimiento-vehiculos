c85e2c09d1e2e265968afbcdeab52b5aa8c1fb2d
Rafael López Torres <rltsistemas@gmail.com>
2017-09-01 14:23:38 +0200

    Added rest service

---
+++ src/main/java/org/geowe/client/shared/rest/sgf/SGFRegisteredPointService.java
@@ -0,0 +1,15 @@
+package org.geowe.client.shared.rest.sgf;
+
+import javax.ws.rs.Path;
+
+/**
+ * Servicio que expone la API de Puntos Registrados por los GPS
+ * 
+ * @author lotor
+ *
+ */
+@Path("/registeredpoints")
+public interface SGFRegisteredPointService {
+
+
+}
---
+++ src/main/java/org/geowe/client/shared/rest/sgf/SGFVehicleService.java
@@ -0,0 +1,16 @@
+package org.geowe.client.shared.rest.sgf;
+
+import javax.ws.rs.Path;
+
+/**
+ * Servicio que expone la API de Vehículos.
+ * 
+ * @author rltorres
+ *
+ */
+@Path("/{compayId}/vehicles")
+public interface SGFVehicleService {
+
+
+
+}
